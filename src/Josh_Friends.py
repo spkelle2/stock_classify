b7a13564bd9d217d98da897df2a2f30915d47454
Josh Weisberg <jiweisb2@Weisberg-Command-Center.local>
2018-04-28 14:31:29 -0700

    upload 1

---
+++ src/Josh_Friends.py
@@ -0,0 +1,104 @@
+import pandas as pd 
+import numpy as np 
+from models import Run, Inflection
+
+
+
+
+
+def Init(stock) :
+	rawData = pd.read_csv(stock + '.csv')
+	rawData2 = pd.read_csv(stock + '-2.csv')
+	length = rawData['Date'].count()
+	rawData.reset_index(drop = True)
+	rawData2.reset_index(drop = True)
+	rawData['Delta'] = rawData2['Adj Close']-rawData['Adj Close'] 
+	print("Read ", stock)
+	return (loop_construct(rawData), rawData)
+
+
+
+def loop_construct(data):
+	last_ = None
+	next_ = None
+	curr = None
+	first = False
+	for i in range(data.shape[0]):
+		if i > 0:
+			last = curr
+			if data['Delta'].iloc[i]*data['Delta'].iloc[i-1]<0:# and  (abs(data['Delta'].iloc[i])>runThreshold or data['Delta'].iloc[i-1]*data['Delta'].iloc[i+1]<0):
+				curr = Inflection(i)
+				curr._prev = last
+				if last != None:
+					last._next = curr
+				if first == False:
+					start = curr
+					first = True
+	return start
+
+
+def CreateRuns(key):
+	runs = []
+	item = stocks[key][0]
+	data = stocks[key][1]
+	while item != None:
+		if abs(data['Delta'].iloc[item.index])<runThreshold and data['Delta'].iloc[ item.index -1  ]*data['Delta'].iloc[item.index+1]>0:
+			item._prev._next = item._next._next
+			item._next._next.prev = item._prev
+			item._next = item._next._next
+		item = item._next
+	item = stocks[key][0]
+	while item != None:
+		inflections = []
+		inflections.append(item)
+		n=0
+		start = item
+		while n<runLength-1 and item!=None:
+			n=n+1
+			inflections.append(item._next)
+			item = item._next
+			finish = item
+		if finish !=None:
+			_run = Run(key, inflections)
+			runs.append(_run)
+		j=0
+		item = start
+		while j<deltaJ and item!=None:
+			j=j+1
+			item = item._next
+	return runs
+
+
+
+
+
+runThreshold = 1
+runLength = 3
+deltaJ = 1
+stocks={} 
+stocks['DIS'] = Init('DIS') #, 'AAPL', 'FOXA', 'CBS', 'TMX']
+#stocks['AAPL']= Init('AAPL')
+
+
+runs={}
+for key in stocks:
+	runs[key] = CreateRuns(key)
+
+
+
+for run in runs['DIS']:
+	print("RUN:")
+	for inflection in run.inflections:
+		print(stocks['DIS'][1]['Adj Close'].iloc[inflection.index])
+	print("\n")
+
+
+
+
+
+
+
+
+
+
+
--- src/models.py
+++ src/models.py
@@ -2,22 +2,24 @@ import pandas as pd
 import numpy as np
 
 
-class Inflection(object):
-    next = None
-    prev = None
 
+class Inflection(object):
     def __init__(self, index):
-        index = index
+        self.index = index
+        self._next = None
+        self._prev = None
+
+
 
 
 class Run(object):
     def __init__(self, stock_id, inflections, orient=None, volatility=None,
                  earnings_per_share=None, averege_prices=None):
-        stock_id = stock_id
-        inflections = inflections
-        orient = orient
-        volatility = volatility
-        earnings_per_share = earnings_per_share
-        ave_price_run = None
-        ave_price_200 = None
-        ave_price_50 = None
+        self.stock_id = stock_id
+        self.inflections = inflections
+        self.orient = orient
+        self.volatility = volatility
+        self.earnings_per_share = earnings_per_share
+        self.ave_price_run = None
+        self.ave_price_200 = None
+        self.ave_price_50 = None
